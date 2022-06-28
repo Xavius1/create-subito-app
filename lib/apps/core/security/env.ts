c850e16f3bee6e3b3e4751f83125ff4e8ff478b1
Xavier <xherriot@protonmail.com>
2022-06-28 12:13:53 +0200

    feat(rabbitmq): interface wip

--- lib/apps/core/security/env.ts
+++ lib/apps/core/security/env.ts
@@ -1,8 +1,8 @@
 import { Env } from 'subito-lib';
 
 const env = Env.getAll([
-  ['APP_ENV', { allow: ['local', 'current', 'develop', 'integration', 'staging', 'production'] }],
-  ['NODE_ENV', { allow: ['development', 'test', 'production'] }],
+  ['APP_ENV'],
+  ['NODE_ENV'],
   ['INTERNAL_GATEWAY', { defaultValue: 'server' }],
   Env.newSecret('JWT_KEY'),
   Env.newSecret('PWD'),
